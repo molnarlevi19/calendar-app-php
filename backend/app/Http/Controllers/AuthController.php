98f50f41e344fc4e66c03ef01d0761eca43c9e00
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-11 10:15:44 +0100

    feat: create new login and logout method for AuthController class

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Request\RegisterRequest;
 use App\Models\User;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,7 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest $request){
+    public function register(RegisterRequest $request): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
 
         $hashPassword = Hash::make($request->input("password"));
         $uuid = Str::orderedUuid();
@@ -25,5 +28,38 @@ class AuthController extends Controller
 
 
         return \response($user,Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        $fields = $request->validate([
+           'email' => 'required|string',
+           'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'token' => $token
+        ];
+
+        return response($response, Response::HTTP_OK);
+    }
+
+    public function logout(): array
+    {
+        auth()->user()->tokens()->delete();
+
+        return[
+            'message' => 'logout',
+        ];
     }
 }
