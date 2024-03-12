bef664396c1b909674673b20b6b4817fdb0f9ddf
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-12 13:31:10 +0100

    feat: create calendar and event models

---
+++ backend/database/migrations/2024_03_12_090456_create_calendars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->bigIncrements('calendar_id');
+            $table->string('calendar_name');
+            $table->string('calendar_description');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calendars');
+    }
+};
