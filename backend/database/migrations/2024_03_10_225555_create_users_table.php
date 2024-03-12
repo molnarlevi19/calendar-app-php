a39daf66c3158236545ae9145bdeaf2dafe290f8
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-12 13:39:07 +0100

    refactor: change uuid column to unique

--- backend/database/migrations/2024_03_10_225555_create_users_table.php
+++ backend/database/migrations/2024_03_10_225555_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->uuid();
+            $table->uuid()->unique();
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
