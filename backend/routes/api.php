eccb51e55dcb7d6b98eab6a09a30dc4ad5b9d1fd
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-12 09:49:09 +0100

    feat: create new Budapest temperature endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TemperatureController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,8 @@ Route::group(['middleware' => 'auth:sanctum'], function ()
 
    Route::post('logout', [AuthController::class, 'logout']);
 });
+
+Route::get('temperature', [TemperatureController::class, 'getTemperature']);
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
