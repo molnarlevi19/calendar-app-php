3a72de08b259abc648a18e1e64f2d907b049c9a1
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-12 09:50:06 +0100

    feat: create new TemperatureController for fetching weather information

---
+++ backend/app/Http/Controllers/TemperatureController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Redis;
+
+class TemperatureController extends Controller
+{
+
+    private static string $apiKey = 'eba53cd393c943e99b7122339231303';
+    private static string $redisKey = 'temperature:budapest';
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getTemperature(): JsonResponse
+    {
+        $temperature = Redis::get(self::$redisKey);
+
+        if($temperature === null) {
+            $temperature = $this->fetchTemperatureFromApi();
+
+            Redis::set(self::$redisKey, $temperature);
+        }
+
+        return response()->json(['temperature' => $temperature]);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    private function fetchTemperatureFromApi(): float
+    {
+        $client = new Client();
+        $response = $client->get('https://api.weatherapi.com/v1/current.json', [
+            'query' => [
+                'key' => self::$apiKey,
+                'q' => 'Budapest',
+                'aqi' => 'no',
+            ]
+        ]);
+        $data = json_decode($response->getBody(), true);
+
+        return $data['current']['temp_c'];
+    }
+}
