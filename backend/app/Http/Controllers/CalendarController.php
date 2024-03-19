9a5043895a208b9f70b06bf53737b3e8eb3af8f3
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-03-19 15:10:36 +0100

    feat: create calendar and eventcontrollers

---
+++ backend/app/Http/Controllers/CalendarController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Calendar;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class CalendarController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'calendar_name' => 'required|string|max:255',
+            'calendar_description' => 'nullable|string|max:255',
+            'user_id' => [
+                'required',
+                'integer',
+                Rule::exists('users', 'id'),
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $calendar = Calendar::create([
+            'calendar_name' => $request->input('calendar_name'),
+            'calendar_description' => $request->input('calendar_description'),
+            'user_id' => $request->input('user_id'),
+        ]);
+
+        return \response()->json([$calendar], Response::HTTP_CREATED);
+    }
+}
