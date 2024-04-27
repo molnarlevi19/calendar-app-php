925d4ccebbe34ad4ef746b16474a772a5bfc9b14
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-04-27 21:23:21 +0200

    refactor: Create a service class for event functionalities

---
+++ backend/app/Services/EventService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class EventService
+{
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(),[
+            'event_title' => 'required|string|max:255',
+            'event_description' => 'nullable|string|max:255',
+            'start_date' => 'nullable|date',
+            'end_date' => 'nullable|date|after_or_equal:start_date',
+            'calendar_id' => [
+                'required',
+                'integer',
+                Rule::exists('calendars', 'calendar_id'),
+            ],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $event = Event::create([
+            'event_title' => $request->input('event_title'),
+            'event_description' => $request->input('event_description'),
+            'start_date' => $request->input('start_date'),
+            'end_date' => $request->input('end_date'),
+            'calendar_id' => $request->input('calendar_id'),
+        ]);
+
+        return \response()->json([$event], Response::HTTP_CREATED);
+    }
+}
