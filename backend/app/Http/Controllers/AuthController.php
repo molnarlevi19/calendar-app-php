409c4ce2d9d985c2a296a78c099ef1303de1472c
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-04-27 21:22:05 +0200

    refactor: Create a service class for authentication

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,64 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Request\RegisterRequest;
-use App\Models\User;
-use Illuminate\Database\QueryException;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use App\Services\AuthService;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+
     public function register(RegisterRequest $request): JsonResponse
     {
-        try {
-            $hashPassword = Hash::make($request->input("password"));
-            $uuid = Str::uuid();
-
-            $user = User::create([
-                'uuid' => $uuid,
-                'username' => $request->input('username'),
-                'email' => $request->input('email'),
-                'password' => $hashPassword,
-            ]);
-
-            return \response()->json([$user], Response::HTTP_CREATED);
-
-        } catch (QueryException $e) {
-
-            if ($e->getCode() === '23000') {
-                return  \response()->json(['error' => 'UUID already exist'], Response::HTTP_CONFLICT);
-            }
-
-            return \response()->json(['error' => 'Database error'], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return $this->authService->register($request);
     }
 
     public function login(Request $request): JsonResponse
     {
-        $credentials = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required|string',
-        ]);
-
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token = $user->createToken('myapptoken')->plainTextToken;
-            return response()->json(['token' => $token, 'user_id' => $user->id], Response::HTTP_OK);
-        }
-
-        return response()->json(['message' => 'Bad credentials'], Response::HTTP_UNAUTHORIZED);
+        return $this->authService->login($request);
     }
 
-    public function logout(): array
+    public function logout(): void
     {
-        auth()->user()->tokens()->delete();
-
-        return [
-            'message' => 'logout',
-        ];
+        $this->authService->logout();
     }
 }
