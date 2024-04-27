246655a19db114beff78bc65e0d0615d11512a88
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2024-04-27 14:26:04 +0200

    refactor: update fillable array in user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -22,6 +23,13 @@ class User extends Authenticatable
         'username',
         'email',
         'password',
+        'surname',
+        'lastname',
+        'birthdate',
+        'UserGender',
+        'phone',
+        'location',
+        'image_id'
     ];
 
     /**
@@ -47,4 +55,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Calendar::class);
     }
+
+    public function profileImage(): HasOne
+    {
+        return $this->hasOne(ProfileImage::class);
+    }
+
 }
